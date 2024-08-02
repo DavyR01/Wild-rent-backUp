8bcee15e2b68db7366d049e095eb2102933f0e6e
Davy ROBERT <davyrobert90@gmail.com>
2024-08-02 11:24:28 +0200

    Recover patterne login

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -122,10 +122,10 @@ const LoginPage = () => {
                   // type="email"
                   {...register("email", {
                     required: "Le mail est requis",
-                  //   pattern: {
-                  //     value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-                  //     message: "Veuillez saisir une adresse e-mail valide",
-                  //   },
+                    pattern: {
+                      value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                      message: "Veuillez saisir une adresse e-mail valide",
+                    },
                   })}
                   className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                   placeholder="name@domain.com"
