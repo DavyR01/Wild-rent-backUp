a365fa8a8b0e8907d5aa19eeb968ec68f4549a40
Davy ROBERT <davyrobert90@gmail.com>
2024-08-09 13:23:24 +0200

    fix 2 integration test RegisterPage.tsx

--- frontend/src/__tests__/RegisterPage.test.tsx
+++ frontend/src/__tests__/RegisterPage.test.tsx
@@ -1,7 +1,8 @@
 import { MockedProvider } from '@apollo/client/testing';
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { CREATE_USER } from 'lib/graphql/mutations';
+import { useRouter } from 'next/router';
 import RegisterPage from 'pages/register';
 
 
@@ -66,42 +67,42 @@ describe('RegisterPage', () => {
     expect(await screen.findByText(/La confirmation de mot de passe est requise/i)).toBeInTheDocument();
   });
 
-//   it('shows error message if passwords do not match', async () => {
-//     render(
-//       <MockedProvider mocks={mocks} addTypename={false}>
-//         <RegisterPage />
-//       </MockedProvider>
-//     );
+  it('shows error message if passwords do not match', async () => {
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <RegisterPage />
+      </MockedProvider>
+    );
 
-//     fireEvent.input(screen.getByLabelText(/Nom d'utilisateur/i), { target: { value: 'testuser' } });
-//     fireEvent.input(screen.getByLabelText(/E-mail/i), { target: { value: 'test@example.com' } });
-//     fireEvent.input(screen.getByLabelText(/Mot de passe/i), { target: { value: 'Password123!' } });
-//     fireEvent.input(screen.getByLabelText(/Confirmez le mot de passe/i), { target: { value: 'Password1234!' } });
+    fireEvent.input(screen.getByLabelText(/Nom d'utilisateur/i), { target: { value: 'user2' } });
+    fireEvent.input(screen.getByLabelText(/E-mail/i), { target: { value: 'user2@dtest.com' } });
+    fireEvent.input(screen.getByLabelText(/Mot de passe/i), { target: { value: 'MyPassword2!' } });
+    fireEvent.input(screen.getByLabelText(/Confirmez le mot de pass/i), { target: { value: 'MyPassword22222!' } });
 
-//     fireEvent.click(screen.getByText(/S'enregistrer/i));
+    fireEvent.click(screen.getByText(/S'enregistrer/i));
 
-//     expect(await screen.findByText(/Les mots de passe ne correspondent pas/i)).toBeInTheDocument();
-//   });
+    expect(await screen.findByText(/Les mots de passe ne correspondent pas/i)).toBeInTheDocument();
+  });
 
-//   it('redirects to login page on successful registration', async () => {
-//     const push = jest.fn();
-//     (useRouter as jest.Mock).mockReturnValue({ push });
+  it('redirects to login page on successful registration', async () => {
+    const mockPush = jest.fn();
+    (useRouter as jest.Mock).mockReturnValue({ push: mockPush });
 
-//     render(
-//       <MockedProvider mocks={mocks} addTypename={false}>
-//         <RegisterPage />
-//       </MockedProvider>
-//     );
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <RegisterPage />
+      </MockedProvider>
+    );
 
-//     fireEvent.input(screen.getByLabelText(/Nom d'utilisateur/i), { target: { value: 'testuser' } });
-//     fireEvent.input(screen.getByLabelText(/E-mail/i), { target: { value: 'test@example.com' } });
-//     fireEvent.input(screen.getByLabelText(/Mot de passe/i), { target: { value: 'Password123!' } });
-//     fireEvent.input(screen.getByLabelText(/Confirmez le mot de passe/i), { target: { value: 'Password123!' } });
+    fireEvent.input(screen.getByLabelText(/Nom d'utilisateur/i), { target: { value: 'user2' } });
+    fireEvent.input(screen.getByLabelText(/E-mail/i), { target: { value: 'user2@dtest.com' } });
+    fireEvent.input(screen.getByLabelText(/Mot de passe/i), { target: { value: 'MyPassword2!' } });
+    fireEvent.input(screen.getByLabelText(/Confirmez le mot de pass/i), { target: { value: 'MyPassword2!' } });
 
-//     fireEvent.click(screen.getByText(/S'enregistrer/i));
+    fireEvent.click(screen.getByText(/S'enregistrer/i));
 
-//     await waitFor(() => {
-//       expect(push).toHaveBeenCalledWith('/login');
-//     });
-//   });
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalledWith('/login');
+    });
+  });
 });
