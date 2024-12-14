a63ee45d89c896787a289d6574b0d2e869f6c42d
Davy <100140271+DavyR01@users.noreply.github.com>
2024-12-14 13:44:43 +0100

    test fix launch prod

--- backend/src/config/datasource.ts
+++ backend/src/config/datasource.ts
@@ -1,32 +1,24 @@
-// import dotenv from "dotenv";
-// import path from "node:path";
 import { DataSource } from "typeorm";
 // import { Migration1721926264177 } from "../database/migrations/1721926264177-migration";
-
-// const envFile =
-//   process.env.NODE_ENV === "production" ? ".env.prod" : ".env.dev";
-
-// dotenv.config({ path: path.join(__dirname, "../../..") });
-
-const {DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME} = process.env
+// import path from 'path';
 
 const dataSource = new DataSource({
   type: "postgres",
   //   url: "postgres://postgres:wildrent@db:5432/postgres",
-  host: DB_HOST,
+  host: "db",
   //   host: "localhost",
-  port: DB_PORT ? parseInt(DB_PORT, 10) : undefined,
+  port: 5432,
   //   port: 5434,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+  username: "postgres",
+  password: "wildrent",
+  database: "postgres",
   entities: ["src/entities/*.ts"],
   synchronize: false,
   logging: ["error", "query"],
-  migrations: ["src/database/migrations/*.ts"], //? Déterminer à quel endroit on enregistre les fichiers de migrations afin de pouvoir les relire.
+  migrations: ["src/database/migrations/*.ts"], // Déterminer à quel endroit on enregistre les fichiers de migrations afin de pouvoir les relire.
   //   migrations: [path.join(__dirname, '../database/migrations/*.ts')],
   //   migrations: [Migration1721926264177],
-  migrationsRun: true //? Assure l'application des migrations non encore appliquées à chaque connexion à la base de données.
+  migrationsRun: true // Assure l'application des migrations non encore appliquées à chaque connexion à la base de données.
 });
 
 export default dataSource;
